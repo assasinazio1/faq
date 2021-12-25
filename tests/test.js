49e4bf9ed8d0a1ba41bd35a259f9c74d3ceee5c8
Stepan Zyablitsev <92617188+assasinazio1@users.noreply.github.com>
2021-12-25 09:58:21 +0500

    Add files via upload

--- tests/test.js
+++ tests/test.js
@@ -1,46 +1,10 @@
-var start = new Date().getTime();
-
-function makeShapeAppear()
-{
-var top = Math.random() * 400;
-var left = Math.random() * 700;
-var width = (Math.random() * 200) + 100;
-if (Math.random() > 0.5) {
-document.getElementById("shape").style.borderRadius = "50%";
-} else {
-document.getElementById("shape").style.borderRadius = "0"
-}
-document.getElementById("shape").style.backgroundColor = "red";
-document.getElementById("shape").style.width = width + "px";
-document.getElementById("shape").style.height = width + "px";
-document.getElementById("shape").style.top = top + "px";
-document.getElementById("shape").style.left = left + "px";
-document.getElementById("shape").style.display = "block"
-start = new Date().getTime();
-}
-
-function appearAfterDelay()
-{
-setTimeout(makeShapeAppear, Math.random() * 1000);
-}
-
-appearAfterDelay()
-
-document.getElementById("shape").onclick = function()
-{
-document.getElementById("shape").style.display = "none";
-var end = new Date().getTime();
-var timeTaken = (end - start) / 1000;
-document.getElementById("timeTaken").innerHTML = timeTaken + "s"
-appearAfterDelay();
-} 
-
 function clickCube() {
     var red = getRandomInt(255);
     var green = getRandomInt(255);
     var blue = getRandomInt(255);
     console.log(red, green, blue);
     document.getElementById("krug").style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
+    document.getElementById("krug").style.borderColor = "rgb(" + red + "," + green + "," + blue + ")";
 
     var stringBoxShadow = "0 0 5px rgba(" + red + "," + green + "," + blue + ", 1), " +
     "0 0 25px rgba(" + red + "," + green + "," + blue + ", 1), " +
@@ -51,9 +15,13 @@ function clickCube() {
 
     document.getElementById("krug").style.boxShadow = stringBoxShadow;
 
+    var px1 = getRandomInt(100);
+    console.log(px1);
+    document.getElementById("krug").style.borderRadius = px1 + "px";
 
 }
 
 function getRandomInt(max) {
     return Math.floor(Math.random()* max);
 }
+
