5533fbded3cc2c0c0293abbc222e24ab91f88c54
Stepan Zyablitsev <92617188+assasinazio1@users.noreply.github.com>
2022-03-11 20:55:12 +0500

    Задачки

---
+++ Задачки/script.js
@@ -0,0 +1,41 @@
+function getResult_1 (string) {
+    return string[0].toUpperCase() + string.substring(1, string.length)
+}
+
+console.log(getResult_1('имя'), '==', 'Имя')
+
+
+
+
+function getResult_2 (array) {
+    var number = 0
+
+// for (let i = 0; i < array.length; i++) {
+// number = number + array[i]    
+// }    
+
+    array.map((a) => {
+        number = number + a
+    })
+
+    return number
+}
+
+console.log(getResult_2([1, 2, 3, 4, 5]), '==', 15)
+console.log(getResult_2([-1, -2, -3, -4, -5]), '==', -15)
+console.log(getResult_2([-2, -2, 0, 4]), '==', 0)
+
+
+
+function getResult_3 (text) {
+    var map = {
+        '2': '!',
+        '3': '!',
+        '5': '!',
+       
+      };
+    
+      return text.replace(/[235]/g, function(m) { return map[m]; });
+    }
+
+console.log(getResult_3('Текст с числами 2 и 3 до 5.'), '==', 'Текст с числами ! и ! до !.')
